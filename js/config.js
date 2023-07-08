eb3c54b49a37e55720b201677e7fd2dd294ad904
Olakunle Ogunlesi <oogunlesi2002@yahoo.co.uk>
2023-07-08 10:09:47 +0000

    config updated: Sat Jul  8 10:09:47 UTC 2023

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_daHMQRHRj', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'lk1l7ooep19rid1olgp2ju37h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_EIAQej7fa',
+        userPoolClientId: 'kf165smllrq1h9u8ve9h06vrf',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
