1cf6149a65fda452f9ab8a722ea914b6020b6095
Olakunle Ogunlesi <oogunlesi2002@yahoo.co.uk>
2023-07-25 14:15:16 +0000

    config updated: Tue Jul 25 14:15:15 UTC 2023

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_EIAQej7fa',
-        userPoolClientId: 'kf165smllrq1h9u8ve9h06vrf',
+        userPoolId: 'us-east-1_W4rYV67P4',
+        userPoolClientId: '5jsag7g8q403ugscd7negf4690',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://etbxmdk0kj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '',
     }
 };
