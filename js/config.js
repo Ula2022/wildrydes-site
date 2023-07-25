2720599250e5986d2393d2fc505d9f67b386ade7
olakunle <oogunlesi2002@yahoo.co.uk>
2023-07-25 16:02:17 +0100

    updated invokeURL

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_daHMQRHRj', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'lk1l7ooep19rid1olgp2ju37h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_W4rYV67P4',
+        userPoolClientId: '5jsag7g8q403ugscd7negf4690',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: ' https://37wdc8ueg7.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://etbxmdk0kj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
